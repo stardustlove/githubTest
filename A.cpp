51cc7e08ce35ae8aa3fd4bba6762c007b3e896ab
Vantablack <2690694169@qq.com>
2025-03-16 20:38:14 +0800

    test push

--- A.cpp
+++ A.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
   cout<<"this is A.cpp"<<endl;
+  cout<<"testing push"<<endl;
   return 0;
 }
--- B.cpp
+++ B.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
   cout<<"this is B.cpp"<<endl;
   cout<<"testing update"<<endl;
+  cout<<"testing push"<<endl;
   return 0;
 }
